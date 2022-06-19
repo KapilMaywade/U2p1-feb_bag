da2d13be4efd84129ce4095b8fa43a6d6c0cf05f
Gautam2s0 <sonkarg117@gmail.com>
2022-06-19 16:10:18 +0530

    final

---
+++ signin.js
@@ -0,0 +1,69 @@
+let form = document.querySelector("#gform");
+form.addEventListener("submit", login);
+let signdta = JSON.parse(localStorage.getItem("userdata")) || [];
+let Sdata = localStorage.getItem("user");
+Sdata = JSON.parse(Sdata) || [];
+function login(event) {
+  event.preventDefault();
+  let inputdata = document.getElementById("gform");
+  let email = inputdata.email.value;
+  let password = inputdata.password.value;
+  let body = document.querySelector("#mess");
+  body.innerHTML = "";
+  let h = document.createElement("h3");
+  let p1 = document.createElement("p");
+  let p2 = document.createElement("p");
+  let object = {
+    email,
+    password,
+  };
+
+
+  let serch = Sdata.filter(function (elem) {
+    return email == elem.email;
+  });
+  if (serch.length == 0 && email != "" && password != "") {
+    p1.innerText =
+      "Sorry, looks like something went wrong. Please correct the following and submit again:";
+    h.innerText = "Sorry, You Don't have an account ";
+    h.style.marginTop = "15px";
+    h.style.color = "red";
+    p1.style.fontFamily = "poppins Sans,sans serif";
+    h.style.fontFamily = "poppins Sans,sans serif";
+    body.append(p1, h);
+    form.reset();
+  } else if ((email == "" && password == "") || email == "" || password == "") {
+    h.innerText = "Please Enter All Credentials";
+    h.style.color = "red";
+    h.style.fontFamily = "poppins Sans,sans serif";
+    body.append(h);
+    form.reset();
+  }
+  if (serch.length > 0) {
+    let flag = false;
+    for (let i = 0; i < data.length; i++) {
+      if (email === data[i].email && password === data[i].password) {
+        flag = true;
+        break;
+      } else {
+        flag = false;
+      }
+    }
+    if (flag == true) {
+      signdta.push(object);
+      console.log(signdta);
+      localStorage.setItem("userdata", JSON.stringify(object));
+      localStorage.setItem("userkey", "Gauti");
+      window.location.href = "navbar.html";
+    } else {
+      p1.innerText = "Sorry, looks like something went wrong. Please correct the following and submit again:";
+      p2.innerText = ". Incorrect email or password.";
+      p2.style.marginTop = "20px";
+      p2.style.fontFamily = "poppins Sans,sans serif";
+      p1.style.fontFamily = "poppins Sans,sans serif";
+      let r = document.querySelector("#gautam>#mess");
+      r.append(p1, p2);
+      form.reset();
+    }
+  }
+}
